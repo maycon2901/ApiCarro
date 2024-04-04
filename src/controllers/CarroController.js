4f431257bd3baa59fd37844139270e2355495f45
Maycon Santos <maycon.santos3@gmail.com>
2024-04-04 10:12:11 -0300

    04.04-01

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -13,5 +13,18 @@ module.exports = {
             });
         }
         res.json(json);
+    },
+
+    buscarUm: async(req, res) => {
+        let json = {error:'', result:{}};
+
+        let codigo = req.params.codigo;
+        let carro = await CarroService.buscarUm(codigo);
+
+        if(carro){
+            json.result = carro;
+        }
+
+        res.json(json);
     }
 }
